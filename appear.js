1a5f869d8c6aa7f8b0e0de3a1e93fe21f659347f
Sh1nju <hello@meganhuey.com>
2014-11-04 05:25:38 -0800

    Smooth scrolling anchor tags
    Smooth scrolling anchor tags in Jquery Expanding content scrolls to
    expand. Changed text in About page.

--- appear.js
+++ appear.js
@@ -1,5 +1,23 @@
 // JavaScript Document
 
+//ANCHOR
+
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
 //SCROLL SNAP
 
 
