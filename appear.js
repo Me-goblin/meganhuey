b511f6baaeaad145833ffb0dc0df54689dcdf697
Sh1nju <hello@meganhuey.com>
2023-08-11 18:35:09 -0700

    fixed typo in Index for vitamins anchor, added vitamins line in appear.js

--- appear.js
+++ appear.js
@@ -218,6 +218,17 @@ $('.organcardsgroup').fadeToggle();
 
 
 $(document).ready(function(){
+$('.vitaminsgroup').hide();
+$('.vitamins').click(function() {
+$('.1, .2, .3, .5, .6, .7, .8, .9, .10, .11, .12, .13, .14, .15, .16, .17, .18, .19, .20 .21, .22, .23, .24').hide();
+$('.vitaminsgroup').fadeToggle();
+});
+
+
+
+});
+
+$(document).ready(function(){
 $('.steamlinegroup').hide();
 $('.steamline').click(function() {
 $('.1, .2, .3, .5, .6, .7, .8, .9, .10, .11, .12, .13, .14, .15, .16, .17, .18, .19, .20 .21, .22, .23, .24').hide();
